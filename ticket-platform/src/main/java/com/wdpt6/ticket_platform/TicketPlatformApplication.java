f4e4b11d8edc0dced04e92492969dfacabe0f0a4
brizzaa <brizzantex@gmail.com>
2025-07-26 18:06:41 +0200

    first commit

---
+++ ticket-platform/src/main/java/com/wdpt6/ticket_platform/TicketPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.wdpt6.ticket_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketPlatformApplication.class, args);
+	}
+}
